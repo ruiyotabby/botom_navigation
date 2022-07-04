e5d4b4b9ea278974d194725ad988b9db11838d4b
Ruiyot Abby <ruiyotabby@gmail.com>
2022-07-04 15:10:25 +0300

    commit
    Signed-off-by: Ruiyot Abby <ruiyotabby@gmail.com>

---
+++ lib/defaults/defaults.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+class Defaults {
+  static final Color? bottomNavItemColor = Colors.blueGrey;
+  static final Color? bottomNavItemSelectorColor = Colors.blue[700];
+
+  static final bottomNavItemText = [
+    'Inbox',
+    'Starred',
+    'Sent',
+    'Drafts',
+    'Trash',
+    'Spam',
+  ];
+
+  static final bottomNavItemIcon = [
+    Icons.inbox,
+    Icons.star,
+    Icons.send,
+    Icons.mail,
+    Icons.delete,
+    Icons.warning_rounded,
+  ];
+}
+
